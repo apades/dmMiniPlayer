bfb5d841e3d4df763e615329083e2c5e35857c5a
apades <apad2@qq.com>
2023-04-20 14:20:14 +0800

    chore: 将injector的运行优先级提高

--- src/contents/inject.ts
+++ src/contents/inject.ts
@@ -4,6 +4,6 @@ import '../inject'
 export const config: PlasmoCSConfig = {
   matches: ['https://www.bilibili.com/*'],
   world: 'MAIN',
-  run_at: 'document_end',
+  run_at: 'document_start',
   all_frames: true,
 }
