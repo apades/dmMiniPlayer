5e2b25fc3cca4dfffff7400669473d8d69ee26bd
apades <36807043+apades@users.noreply.github.com>
2023-06-17 15:55:19 +0800

    test: echarts测试

--- src/tabs/performance.tsx
+++ src/tabs/performance.tsx
@@ -1,9 +1,12 @@
 import { listen as portListen, getPort } from '@plasmohq/messaging/port'
 // import { getPort } from '@plasmohq/messaging/background'
 import { useOnce } from '@root/hook'
-import { FC } from 'react'
+import { FC, useRef } from 'react'
+import * as eCharts from 'echarts'
+import { ECOption } from '@root/utils/echarts'
 
 const Page_performance: FC = (props) => {
+  const chartContainerRef = useRef<HTMLDivElement>()
   useOnce(async () => {
     let port = await getPort('page-performance')
     // 不支持往回传数据
@@ -21,7 +24,57 @@ const Page_performance: FC = (props) => {
     // })
   })
 
-  return <div>Page_performance</div>
+  useOnce(() => {
+    const chart = eCharts.init(chartContainerRef.current)
+    const option: ECOption = {
+      title: {
+        text: 'ECharts 入门示例',
+      },
+      tooltip: {},
+      xAxis: {
+        // data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
+        data: ['0', '1', '2', '3', '4', '5'],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '无限制刷新率',
+          type: 'line',
+          data: [5, 20, 36, 10, 10, 20],
+          smooth: true,
+        },
+        {
+          name: '限制刷新率',
+          type: 'line',
+          data: [
+            {
+              value: 35,
+              itemStyle: { color: 'red' },
+              symbolSize: 8,
+              symbol: 'circle',
+            },
+            { value: 35, itemStyle: { color: 'red' }, symbol: 'none' },
+            { value: 20, itemStyle: { color: 'red' } },
+            20,
+            5,
+            { value: 30, itemStyle: { color: 'red' } },
+          ],
+          smooth: true,
+        },
+      ],
+    }
+
+    chart.setOption(option)
+    // chart.appendData({seriesIndex:})
+    // console.log('chartContainerRef', chartContainerRef.current)
+  })
+
+  return (
+    <div>
+      <p>Page_performance</p>
+      <div style={{ height: 300 }} ref={chartContainerRef}></div>
+    </div>
+  )
 }
 
 export default Page_performance
---
+++ src/utils/echarts.ts
@@ -0,0 +1,26 @@
+import * as echarts from 'echarts'
+import type {
+  // 系列类型的定义后缀都为 SeriesOption
+  BarSeriesOption,
+  LineSeriesOption,
+} from 'echarts/charts'
+import type {
+  // 组件类型的定义后缀都为 ComponentOption
+  TitleComponentOption,
+  TooltipComponentOption,
+  GridComponentOption,
+  DatasetComponentOption,
+} from 'echarts/components'
+import type { ComposeOption } from 'echarts/core'
+
+// 通过 ComposeOption 来组合出一个只有必须组件和图表的 Option 类型
+export type ECOption = ComposeOption<
+  | BarSeriesOption
+  | LineSeriesOption
+  | TitleComponentOption
+  | TooltipComponentOption
+  | GridComponentOption
+  | DatasetComponentOption
+>
+
+export default echarts
