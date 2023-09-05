d0e6521415263f749532594f20edd1eb3e2d6f19
apades <36807043+apades@users.noreply.github.com>
2023-09-05 16:42:54 +0800

    chore: 去掉原生pip注入

--- src/inject/injectPIPFunction.ts
+++ src/inject/injectPIPFunction.ts
@@ -1,11 +1,12 @@
 import { sendMessage_inject } from './injectListener'
 
+// TODO 用开关控制
 // Element.prototype.attachShadow = ()=>{}
-let origin = HTMLVideoElement.prototype.requestPictureInPicture
+// let origin = HTMLVideoElement.prototype.requestPictureInPicture
 
-console.log('inject requestPictureInPicture')
-HTMLVideoElement.prototype.requestPictureInPicture = function () {
-  //   return origin.bind(this)({ mode: 'open' })
-  console.log('player-startPIPPlay')
-  sendMessage_inject('start-PIP')
-} as any
+// console.log('inject requestPictureInPicture')
+// HTMLVideoElement.prototype.requestPictureInPicture = function () {
+//   //   return origin.bind(this)({ mode: 'open' })
+//   console.log('player-startPIPPlay')
+//   sendMessage_inject('start-PIP')
+// } as any
