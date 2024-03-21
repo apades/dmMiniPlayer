43f4310ac1744b2dfa6eb4453bdf99905ed3a0d8
apades <36807043+apades@users.noreply.github.com>
2024-03-22 00:38:15 +0800

    fix: bilibili直播弹幕不生效问题

--- src/danmaku/bilibili/liveBarrageClient/index.ts
+++ src/danmaku/bilibili/liveBarrageClient/index.ts
@@ -16,6 +16,21 @@ const getRoomid = async (short: number) => {
   return room_id
 }
 
+export const getConf = async (roomid: number) => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const raw = await fetch(
+    `https://api.live.bilibili.com/xlive/web-room/v1/index/getDanmuInfo?id=${roomid}`
+  ).then((w) => w.json())
+  const {
+    data: {
+      token: key,
+      host_list: [{ host, wss_port: port }],
+    },
+  } = raw
+  const address = `wss://${host}/sub`
+  return { key, host, port, address, raw }
+}
+
 export default class BilibiliLiveBarrageClient extends BarrageClient {
   ws: LiveWS
   constructor(public id: number) {
@@ -25,9 +40,17 @@ export default class BilibiliLiveBarrageClient extends BarrageClient {
 
   async init(id: number) {
     const realRoomId = await getRoomid(id)
-    console.log('realRoomId', realRoomId)
-    this.ws = new LiveWS(realRoomId)
-    this.ws.on('open', () => console.log('Connection is established'))
+    const conf = await getConf(545318)
+    const address = `wss://${conf.host}:${conf.port}/sub`
+
+    console.log('realRoomId', realRoomId, conf)
+    this.ws = new LiveWS(realRoomId, {
+      protover: 3,
+      address,
+      key: conf.key,
+    })
+    this.ws.on('open', () => console.log('弹幕ws连接成功'))
+    this.ws.on('close', () => console.log('弹幕ws断开'))
     // Connection is established
     this.ws.on('live', () => {
       this.ws.on('heartbeat', console.log)
