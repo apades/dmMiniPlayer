b56571b60b2acfbaaed23c94a41b5e02c20f5583
apades <36807043+apades@users.noreply.github.com>
2024-09-01 14:17:26 +0800

    chore: 调整openPlayer的trust action检测

--- src/contents/main.ts
+++ src/contents/main.ts
@@ -1,67 +1,68 @@
 import getWebProvider from '../web-provider/getWebProvider'
 import { onMessage as onBgMessage } from 'webext-bridge/content-script'
-import AsyncLock from '@root/utils/AsyncLock'
 import { onMessage } from '@root/inject/contentSender'
-import { onceCall } from '@root/utils'
+import { createElement, dq1Adv, onceCall } from '@root/utils'
+import { WebProvider } from '@root/core/WebProvider'
 
 console.log('run content')
 
 let provider = onceCall(() => {
-  let provider = getWebProvider()
+  let provider = getWebProvider()!
   window.provider = provider
   return provider
 })
 
-let hasClickPage = false,
-  isWaiting = false
-let clickLock = new AsyncLock()
-window.addEventListener('click', (e) => {
-  if (!e.isTrusted) return
-  hasClickPage = true
-  clickLock.ok()
-})
-
-const handleBlur = () => {
-  console.log('blur')
-  hasClickPage = false
-  clickLock.reWaiting()
-  window.removeEventListener('blur', handleBlur)
-}
-
-const openPIP = async () => {
-  if (isWaiting) return
-  isWaiting = true
-  await clickLock.waiting()
-  isWaiting = false
-  provider()?.openPlayer()
-
-  window.addEventListener('blur', handleBlur)
-}
-onBgMessage('player-startPIPPlay', async (req) => {
-  if (!hasClickPage) {
-    const coverEl = document.createElement('div')
+const waitingPageActive = async () => {
+  console.log(
+    'navigator.userActivation.isActive',
+    navigator.userActivation.isActive
+  )
+  if (navigator.userActivation.isActive) return
+  return new Promise<void>((res) => {
+    const coverEl = createElement('div')
     ;(coverEl as any).style =
       'width:100%;height:100%;position:fixed;top:0;left:0;z-index:9999999;'
     document.body.appendChild(coverEl)
     coverEl.addEventListener('click', (e) => {
       if (!e.isTrusted) return
+      if (!navigator.userActivation.isActive) return
       document.body.removeChild(coverEl)
-      openPIP()
+      res()
+    })
+  })
+}
+
+let isWaiting = false
+
+const openPlayer = async (props?: Parameters<WebProvider['openPlayer']>[0]) => {
+  // 避免多次open
+  if (isWaiting) return
+  isWaiting = true
+  await provider()?.openPlayer(props)
+  isWaiting = false
+}
+
+// 从popup点击过来的消息
+onBgMessage('player-startPIPPlay', async (req) => {
+  const hasVideo = !!dq1Adv('video')
+  if (!hasVideo) {
+    return { state: 'error', type: 'no-video' }
+  }
+
+  if (!navigator.userActivation.isActive) {
+    waitingPageActive().then(() => {
+      openPlayer()
     })
     return { state: 'error', type: 'click-page' }
   }
-  openPIP()
+
+  openPlayer()
   return { state: 'ok' }
 })
 
+// 从子iframe里过来的消息
 onMessage('start-PIP', (data) => {
-  // provider().startPIPPlay({ videoEl: data.videoEl })
-  // const provider = new NewBilibiliVideoProvider()
-  const provider = getWebProvider()
-  if (!provider) throw new Error('找不到对应的provider')
-  window.provider = provider
-
-  provider.openPlayer({ videoEl: data.videoEl })
+  openPlayer({ videoEl: data.videoEl })
 })
 
 try {
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -5,8 +5,14 @@ import Browser from 'webextension-polyfill'
 import { t } from '../utils/i18n'
 import { createRoot } from 'react-dom/client'
 
+const errorTypeMap: Record<string, string> = {
+  'click-page': t('popup.tips'),
+  'no-video': t('popup.noVideo'),
+}
+
 const Page_popup: FC = () => {
-  let [isClickError, setClickError] = useState(false)
+  const [errorType, setErrorType] = useState('')
+
   useOnce(async () => {
     const tabs = await Browser.tabs.query({ active: true, currentWindow: true })
     if (!tabs.length) return
@@ -16,24 +22,25 @@ const Page_popup: FC = () => {
         name: 'player-startPIPPlay',
       },
       {
-        tabId: tabs[0].id,
+        tabId: tabs[0].id!,
         context: 'content-script',
       }
     ).then((res: any) => {
       if (res.state == 'ok') return window.close()
       switch (res.type) {
-        case 'click-page': {
-          setClickError(true)
-          return
+        case 'click-page':
+        case 'no-video': {
+          setErrorType(res.type)
+          break
         }
       }
     })
   })
   return (
-    isClickError && (
-      <div style={{ width: 100, padding: 6 }}>{t('popup.tips')}</div>
+    errorType && (
+      <div style={{ width: 100, padding: 6 }}>{errorTypeMap[errorType]}</div>
     )
   )
 }
 
-createRoot(document.getElementById('app')).render(<Page_popup />)
+createRoot(document.getElementById('app')!).render(<Page_popup />)
