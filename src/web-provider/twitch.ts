8ab7a2068e75113790a6df76b28396966f04aa6a
apades <36807043+apades@users.noreply.github.com>
2024-11-10 18:38:32 +0800

    fix: twitch弹幕失效

--- src/web-provider/twitch.ts
+++ src/web-provider/twitch.ts
@@ -8,7 +8,7 @@ export default class TwitchProvider extends HtmlDanmakuProvider {
       container: dq1<HTMLDivElement>(
         '.chat-scrollable-area__message-container'
       )!,
-      child: '.chat-line__message',
+      child: '*',
       text: '.chat-line__message-container .chat-line__username-container ~ span:last-of-type',
     }
   }
