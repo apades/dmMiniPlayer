78b454b08f85f0ad2bf1f16cecf7f5fcba4eac58
apades <36807043+apades@users.noreply.github.com>
2024-11-26 10:31:39 +0800

    scripts: workflows的auto release增加Link到md文档

--- scripts/getChangeLog.mjs
+++ scripts/getChangeLog.mjs
@@ -3,6 +3,10 @@ import packageJson from '../package.json'
 
 const version = packageJson.version
 
+const url = 'https://github.com/apades/dmMiniPlayer/blob/main/docs/changeLog'
+
 console.log(getChangeLog(version))
+console.log(`[Link](${url}.md#${version.replaceAll('.', '')})`)
 console.log('---------')
 console.log(getChangeLog(version, 'zh'))
+console.log(`[Link](${url}-zh.md#${version.replaceAll('.', '')})`)
