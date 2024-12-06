347403f7feda62d310603aa31057320e66d6089c
apades <36807043+apades@users.noreply.github.com>
2024-12-06 10:38:44 +0800

    script: md解析的changeLog格式错误问题

--- scripts/getChangeLog.mjs
+++ scripts/getChangeLog.mjs
@@ -7,6 +7,7 @@ const url = 'https://github.com/apades/dmMiniPlayer/blob/main/docs/changeLog'
 
 console.log(getChangeLog(version) || 'Small update')
 console.log(`[Link](${url}.md#${version.replaceAll('.', '')})`)
+console.log('')
 console.log('---------')
 console.log(getChangeLog(version, 'zh') || '小更新')
 console.log(`[Link](${url}-zh.md#${version.replaceAll('.', '')})`)
