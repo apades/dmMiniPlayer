821ce763c5006bd786442887745bdb714b893602
apades <36807043+apades@users.noreply.github.com>
2024-12-12 12:53:55 +0800

    fix: changLog pre line

--- src/components/FloatButton.tsx
+++ src/components/FloatButton.tsx
@@ -485,7 +485,7 @@ const FloatButton: FC<Props> = (props) => {
                     '--x': '0',
                   }}
                 >
-                  <div className="p-1 text-left whitespace-pre">
+                  <div className="p-1 text-left whitespace-pre-wrap">
                     NEW:
                     <br />
                     {changeLog}
