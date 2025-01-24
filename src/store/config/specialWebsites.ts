02b19e9ff4d5f836ffb617a88fe5dedfd1e61f37
apades <36807043+apades@users.noreply.github.com>
2025-01-24 09:53:05 +0800

    todo

--- src/store/config/specialWebsites.ts
+++ src/store/config/specialWebsites.ts
@@ -4,6 +4,15 @@ import { t } from '@root/utils/i18n'
 const category = t('settingPanel.specialWebsites')
 const config: typeof _config = (props) => ({ ...props, category })
 
+const websiteDanmakuSetting = {
+  danmaku_container: '',
+  danmaku_child: '',
+  danmaku_text: '',
+
+  sender_input: '',
+  sender_button: '',
+}
+
 const config_specialWebsites = {
   biliVideoDansFromBiliEvaolved: config({
     defaultValue: false,
@@ -21,6 +30,33 @@ const config_specialWebsites = {
   youtube_mergeSubtitleAtSimilarTimes: config({
     defaultValue: true,
     label: t('settingPanel.youtube_mergeSubtitleAtSimilarTimes'),
+  }),
+
+  websiteDanmakuSetting: config<Record<string, typeof websiteDanmakuSetting>>({
+    type: 'map',
+    defaultValue: {
+      'live.douyin.com': {
+        danmaku_container: '.webcast-chatroom___items>div:first-child',
+        danmaku_child: '.webcast-chatroom___item',
+        danmaku_text: '.webcast-chatroom___content-with-emoji-text',
+        sender_input: '.webcast-chatroom___input-container textarea',
+        sender_button: '.webcast-chatroom___send-btn',
+      },
+      // twitch视频
+      // 'www.twitch.tv/video/': {},
+      // twitch直播
+      '/^www\\.twitch\\.tv\\/(?!video\\/).*$/': {
+        danmaku_container: '.chat-scrollable-area__message-container',
+        danmaku_child: '*',
+        danmaku_text:
+          '.chat-line__message-container .chat-line__username-container ~ span:last-of-type',
+        sender_input: '[data-a-target="chat-input"]',
+        sender_button: '[data-a-target="chat-send-button"]',
+      },
+    },
+    defaultItem: websiteDanmakuSetting,
+    label: 'Website danmaku setting',
+    desc: 'Set HTML danmaku and sender by querySelector',
   }),
   // biliLiveSide: config({
   //   defaultValue: false,
