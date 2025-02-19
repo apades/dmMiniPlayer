892eb173f4674d6fdbef50fd29448861efb3eba3
apades <36807043+apades@users.noreply.github.com>
2025-02-20 00:40:10 +0800

    chore: 调整release log

--- scripts/getChangeLog.mjs
+++ scripts/getChangeLog.mjs
@@ -8,6 +8,6 @@ const url = 'https://github.com/apades/dmMiniPlayer/blob/main/docs/changeLog'
 console.log(getChangeLog(version) || 'Small update')
 console.log(`[More](${url}.md#${version.replaceAll('.', '')})`)
 console.log('')
-console.log('---------')
+console.log('### ---')
 console.log(getChangeLog(version, 'zh') || '小更新')
 console.log(`[More](${url}-zh.md#${version.replaceAll('.', '')})`)
