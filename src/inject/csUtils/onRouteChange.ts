b39a2d7882dc1e3efa7f9a3bc6ccece5c24934b3
apades <36807043+apades@users.noreply.github.com>
2025-05-15 11:45:34 +0800

    fix: 替换模式关闭后close事件没有如期触发

--- src/inject/csUtils/onRouteChange.ts
+++ src/inject/csUtils/onRouteChange.ts
@@ -1,4 +1,5 @@
 import { wait } from '@root/utils'
+import Events2 from '@root/utils/Events2'
 import { onMessage, sendMessage } from '../contentSender'
 
 onMessage('inject-api:onTrigger', (data) => {
@@ -6,23 +7,19 @@ onMessage('inject-api:onTrigger', (data) => {
   console.log('切换了路由 history')
 
   wait(0).then(() => {
-    callbacks.forEach((cb) => cb(location.pathname))
+    e.emit('cb', location.pathname)
   })
 })
 window.addEventListener('popstate', () => {
   console.log('切换了路由 popstate')
-  callbacks.forEach((cb) => cb(location.pathname))
+  e.emit('cb', location.pathname)
 })
 
-const callbacks: ((pathname: string) => void)[] = []
+const e = new Events2<{ cb: string }>()
+// const callbacks: ((pathname: string) => void)[] = []
 /**
  * @returns {Function} 取消监听onRouteChange
  */
 export default function onRouteChange(callback: (pathname: string) => void) {
-  callbacks.push(callback)
-  const unListen = () => {
-    callbacks.slice(callbacks.findIndex((c) => c == callback))
-  }
-
-  return unListen
+  return e.on2('cb', callback)
 }
